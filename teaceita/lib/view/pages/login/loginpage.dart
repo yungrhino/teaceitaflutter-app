c1065bedcac13d4730d5b7e47e3c466dab3d73e3
Eduardo Junior (@yungrhino_) <makedabber.oficial@gmail.com>
2024-08-29 02:29:25 -0300

    código simplificado & botões

--- teaceita/lib/view/pages/login/loginpage.dart
+++ teaceita/lib/view/pages/login/loginpage.dart
@@ -9,154 +9,191 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final Color primaryColor = Color.fromRGBO(74, 173, 101, 1);
+  final Color secondaryColor = Colors.black;
+  final Color whiteColor = Colors.white;
+
+  TextStyle _headerStyle(double fontSize, FontWeight fontWeight) {
+    return TextStyle(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: secondaryColor,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
-        alignment: Alignment.topCenter, // Ajusta o alinhamento do container
+        alignment: Alignment.topCenter,
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: 20.0), // Adiciona padding horizontal geral
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  width: 250, // Define a largura desejada
-                  height: 250, // Define a altura desejada
-                  child: FittedBox(
-                    fit: BoxFit
-                        .fill, // Garante que a imagem preencha todo o espaço do SizedBox
-                    child: Image.asset('assets/images/teaceita.png'),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                  height: 10), // Espaçamento entre a imagem e o texto
-              Align(
-                alignment: Alignment.center,
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'TEA',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 45,
-                          color: Colors.black, // Cor do texto
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'ceita',
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 45,
-                          color: Colors.black, // Cor do texto
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                  height:
-                      30), // Espaçamento entre o texto "TEAceita" e o restante do código
-              Text(
-                'Acesse',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-              ),
-              const SizedBox(
-                  height: 10), // Espaçamento entre o título e o texto abaixo
-              RichText(
-                text: TextSpan(
-                  text:
-                      'Com número de telefone ou nome de usuário e senha para entrar',
-                  style: TextStyle(
-                    color: Colors.black, // Ajusta a cor do texto
-                    fontSize: 25,
-                  ),
-                ),
-                textAlign: TextAlign.left,
-              ),
-              const SizedBox(
-                  height:
-                      20), // Espaçamento entre o texto e o primeiro campo de entrada
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Digite seu usuário:',
-                      style: TextStyle(
-                          color: Colors.black54, fontWeight: FontWeight.normal),
-                    ),
-                    Inputtext(
-                      suffixIcon: Icon(Icons.person),
-                      visibility: true,
-                      text: 'Número de telefone ou nome de usuário',
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Digite sua senha:',
-                      style: TextStyle(
-                          color: Colors.black54, fontWeight: FontWeight.normal),
-                    ),
-                    Inputtext(
-                      suffixIcon: Icon(Icons.remove_red_eye_rounded),
-                      visibility: false,
-                      text: 'Senha',
-                      isPassword: true, // Define que este campo é uma senha
-                    ),
-                  ],
-                ),
-              ),
-              const Text(
-                'Esqueci a senha?',
-                style: TextStyle(
-                    color: Color.fromRGBO(74, 173, 101, 100),
-                    fontWeight: FontWeight.w300),
-              ),
-
-              TextButton(onPressed: () {}, child: Text('Cadastrar')),
-
-              Row(
-                children: [
-                  Expanded(
-                      child: Divider(
-                    color: Color.fromRGBO(74, 173, 101, 100),
-                    thickness: 2,
-                  )),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Text(
-                      'Ou acesse com',
-                      style: TextStyle(
-                          color: Color.fromRGBO(74, 173, 101, 100),
-                          fontWeight: FontWeight.w300),
-                    ),
-                  ),
-                  Expanded(
-                      child: Divider(
-                    color: Color.fromRGBO(74, 173, 101, 100),
-                    thickness: 2,
-                  ))
-                ],
-              )
+              _buildLogo(),
+              const SizedBox(height: 10),
+              _buildTitle(),
+              const SizedBox(height: 30),
+              _buildSubtitle(),
+              const SizedBox(height: 20),
+              _buildInputField('Digite seu usuário:', 'Número de telefone ou nome de usuário', Icons.person),
+              _buildInputField('Digite sua senha:', 'Senha', Icons.remove_red_eye_rounded, isPassword: true),
+              _buildForgotPasswordText(),
+              const SizedBox(height: 20),
+              _buildActionButtons(),
+              const SizedBox(height: 20),
+              _buildDividerWithText('Ou acesse com'),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: 250,
+        height: 250,
+        child: FittedBox(
+          fit: BoxFit.fill,
+          child: Image.asset('assets/images/teaceita.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Align(
+      alignment: Alignment.center,
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          children: [
+            TextSpan(text: 'TEA', style: _headerStyle(45, FontWeight.bold)),
+            TextSpan(text: 'ceita', style: _headerStyle(45, FontWeight.normal)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSubtitle() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Acesse', style: _headerStyle(35, FontWeight.bold)),
+        const SizedBox(height: 10),
+        RichText(
+          text: TextSpan(
+            text: 'Com número de telefone ou nome de usuário e senha para entrar',
+            style: _headerStyle(25, FontWeight.normal),
+          ),
+          textAlign: TextAlign.left,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildInputField(String label, String hint, IconData suffixIcon, {bool isPassword = false}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label, style: TextStyle(color: Colors.black54)),
+          Inputtext(
+            suffixIcon: Icon(suffixIcon),
+            visibility: !isPassword,
+            text: hint,
+            isPassword: isPassword,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildForgotPasswordText() {
+    return const Text(
+      'Esqueceu a senha?',
+      style: TextStyle(color: Color.fromRGBO(74, 173, 101, 100), fontWeight: FontWeight.w300),
+    );
+  }
+
+  Widget _buildActionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildButton('Cadastrar', whiteColor, primaryColor),
+        const SizedBox(width: 20),
+        VerticalDivider(color: primaryColor, thickness: 2),
+        const SizedBox(width: 45),
+        _buildButton('Acessar', primaryColor, whiteColor, isElevated: true),
+      ],
+    );
+  }
+
+  Widget _buildButton(String text, Color backgroundColor, Color textColor, {bool isElevated = false}) {
+    return isElevated
+        ? ElevatedButton(
+            onPressed: () {
+              // Ação do botão de acessar
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(backgroundColor),
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              padding: WidgetStateProperty.all<EdgeInsets>(
+                EdgeInsets.symmetric(vertical: 20.0, horizontal: 55.0),
+              ),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
+            ),
+          )
+        : TextButton(
+            onPressed: () {
+              // Ação do botão de cadastrar
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(backgroundColor),
+              side: WidgetStateProperty.all<BorderSide>(
+                BorderSide(color: primaryColor, width: 2),
+              ),
+              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              padding: WidgetStateProperty.all<EdgeInsets>(
+                EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              ),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
+            ),
+          );
+  }
+
+  Widget _buildDividerWithText(String text) {
+    return Row(
+      children: [
+        Expanded(child: Divider(color: Color.fromRGBO(74, 173, 101, 100), thickness: 2)),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(text, style: TextStyle(color: Color.fromRGBO(74, 173, 101, 100), fontWeight: FontWeight.w300)),
+        ),
+        Expanded(child: Divider(color: Color.fromRGBO(74, 173, 101, 100), thickness: 2)),
+      ],
     );
   }
 }
